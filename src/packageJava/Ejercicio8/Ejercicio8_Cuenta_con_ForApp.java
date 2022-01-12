ae847df0b49eea63d8673af345c89968fe73291e
IsaDevs <isabel.calzadilla.18@gmail.com>
2022-01-12 11:24:51 +0100

    Exercises 8, counter with for

---
+++ src/packageJava/Ejercicio8/Ejercicio8_Cuenta_con_ForApp.java
@@ -0,0 +1,16 @@
+package packageJava.Ejercicio8;
+
+/**@author isabel Calzadilla
+ * Ejercicio 7: control de flujo
+ * **/
+public class Ejercicio8_Cuenta_con_ForApp {
+
+	public static void main(String[] args) {
+		//8)Haz el mismo ejercicio anterior con un bucle for
+
+		for(int i = 1; i <=100; i++) {
+			System.out.println("El valor de número en ésta vuelta  es : " + i);
+		}
+	}
+
+}
