dc997ac3f28882d4efd3aa729d1b0e69e30ff132
IsaDevs <isabel.calzadilla.18@gmail.com>
2022-01-12 09:40:58 +0100

    Ejercicio 3

---
+++ src/packageJava/Ejercicio3/Ejercicio3App.java
@@ -0,0 +1,22 @@
+package packageJava.Ejercicio3;
+
+import javax.swing.JOptionPane;
+
+/**@author isabel Calzadilla
+ * Ejercicio 1: control de flujo
+ * **/
+
+public class Ejercicio3App {
+
+	public static void main(String[] args) {
+		/*3)
+Modifica la aplicación anterior, para que nos pida el nombre que queremos introducir
+(recuerda usar JOptionPane*/
+
+String name;
+		
+		name = JOptionPane.showInputDialog("Indica tu nombre, por favor : ");
+		JOptionPane.showMessageDialog(null, "Bienvenido  " + name);
+	}
+
+}
