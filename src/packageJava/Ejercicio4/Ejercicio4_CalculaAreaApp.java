0f14592c57cb7d62fd5ce909ff0a648a83af8da2
IsaDevs <isabel.calzadilla.18@gmail.com>
2022-01-12 10:57:38 +0100

    Exercise 4 calculate area

---
+++ src/packageJava/Ejercicio4/Ejercicio4_CalculaAreaApp.java
@@ -0,0 +1,25 @@
+package packageJava.Ejercicio4;
+
+import javax.swing.*;
+
+/**@author isabel Calzadilla
+ * Ejercicio 4: control de flujo
+ * **/
+public class Ejercicio4_CalculaAreaApp {
+
+	public static void main(String[] args) {
+		/*4)
+Haz una aplicación que calcule el área de un circulo (pi*R 2 ). El radio se pedirá por teclado
+(recuerda pasar de String a double con Double.parseDouble ). Usa la constante PI y el
+método pow de Math*/
+		
+		final double PI = 3.14;
+		double radio;
+		
+		String radius = JOptionPane.showInputDialog("Indique el radio : ");
+		
+		radio = Double.parseDouble(radius);
+		JOptionPane.showMessageDialog(null, "El área del circulo con radio " + radio + " es igual a : " + ((PI * radio) * 2));
+	}
+
+}
